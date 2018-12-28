247bf61fbe3517c22edfa8655aa4f3e237655e95
ilgmi <michiilg@web.de>
2018-12-28 21:30:19 +0100

    Import Json File

--- classification_snips/manager/src/app/nlu-manage/import/import/import.component.ts
+++ classification_snips/manager/src/app/nlu-manage/import/import/import.component.ts
@@ -11,6 +11,8 @@ export class ImportComponent implements OnInit {
 
   public hasValidJson = false;
 
+  public label = 'Snips Json File';
+
   constructor() { }
 
   ngOnInit() {
@@ -19,17 +21,34 @@ export class ImportComponent implements OnInit {
 
   private checkJson(content){
     // TODO: Check content for valid Objects
+
+    this.hasValidJson = true;
   }
 
   public useFile(files: File[]) {
     console.log(files);
     if(files.length > 0){
       const file = files[0];
+      this.label = file.name;
       const fileReader = new FileReader();
       fileReader.onload = (e) => {
         this.checkJson(JSON.parse(<string>fileReader.result));
       };
       fileReader.readAsText(file);
     }
+  }
+
+  replaceData() {
+    // TODO: Add Modal
+
+
+    // TODO: Create Intents and Entities and replace current data
+  }
+
+  addData() {
+    // TODO: Add Modal
+
+
+    // TODO: Create Intents an Entities and add Data to current Trainings-Data
   }
 }
