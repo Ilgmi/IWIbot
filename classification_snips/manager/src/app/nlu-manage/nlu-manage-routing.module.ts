9edb2742d3ab8eb65ac52f66af7824685f45c731
ilgmi <michiilg@web.de>
2019-01-20 20:46:07 +0100

    Sentences hinzufügen

--- classification_snips/manager/src/app/nlu-manage/nlu-manage-routing.module.ts
+++ classification_snips/manager/src/app/nlu-manage/nlu-manage-routing.module.ts
@@ -7,6 +7,7 @@ import {EntityListComponent} from './entity/entity-list/entity-list.component';
 import {EntityEditComponent} from './entity/entity-edit/entity-edit.component';
 import {ImportComponent} from './import/import/import.component';
 import {TrainingComponent} from './training/training.component';
+import {SentenceComponent} from './sentence/sentence.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'entity/:name', component: EntityEditComponent},
   { path: 'import', component: ImportComponent},
   { path: 'training', component: TrainingComponent},
+  { path: 'sentences', component: SentenceComponent},
   ];
 
 @NgModule({
